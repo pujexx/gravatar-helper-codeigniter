1a299ecb4dd1c129a2d42014a1147f15ab184ca2
puji rahmadiyanto <puji.rahmadiyanto@gmail.com>
2011-07-30 19:30:57 +0700

    dokumentasi

--- gravatar_helper.php
+++ gravatar_helper.php
@@ -1,7 +1,21 @@
 <?php 
 
+/**
+ * Get image form gravatar.com
+ *
+ * @parameter email  	email address
+ * @parameter with 		with image
+ * sample usage 
+ *********************************************************************
+ * $this->load->helper("gravatar");
+ * echo gravatar_img("example@example.com");
+ * //or with width value 
+ * echo gravatar_img("example@example.com",20);
+ *********************************************************************
+ */
+
 if(!function_exists('gravatar_img')){
-	function gravatar_img($email,$width=""){
+	function gravatar_img($email="",$width=""){
 		$src= trim($email);
 		$src = md5(strtolower($src));
 		if($width==""){
